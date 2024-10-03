cb137ae90f21aebf1214e76830fd019cab699ebc
Vinodnagulkar <42966529+Vinodnagulkar@users.noreply.github.com>
2024-10-03 20:45:56 +0530

    Update PmsURI.java

--- ProjectManagementSystem/src/main/java/com/pms/util/PmsURI.java
+++ ProjectManagementSystem/src/main/java/com/pms/util/PmsURI.java
@@ -2,9 +2,9 @@ package com.pms.util;
 
 public class PmsURI 
 {
-	public static final String ADD_PROJECT="/pms/project/addProject";
-	public static final String GET_ALL_PROJECTS="/pms/project/getAllProjects";
-	public static final String GET_PROJECT="/pms/project/getProjectById/{id}";
-	public static final String UPDATE_PROJECT="/pms/project/updateProject/{id}";
-	public static final String DELETE_PROJECT="/pms/project/deleteProject/{id}";
+	public static final String ADD_PROJECT="/pms/project";
+	public static final String GET_ALL_PROJECTS="/pms/project";
+	public static final String GET_PROJECT="/pms/project/{id}";
+	public static final String UPDATE_PROJECT="/pms/project/{id}";
+	public static final String DELETE_PROJECT="/pms/project/{id}";
 }
